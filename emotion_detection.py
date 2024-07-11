b4be44e2f9fcb26203abb69120b0b774ed70fcfb
b-aeo <bradespinoza.o@hotmail.com>
2024-07-11 15:42:42 -0400

    Started on emotion detector code

---
+++ emotion_detection.py
@@ -0,0 +1,15 @@
+''' Function for calling the IBM AI for emotion detection'''
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    '''This is the function script, uses the post method from the requests method'''
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    text = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, headers = headers, json = text, timeout = 30)
+    json_response = json.loads(response.text)
+    format_response = json_response['emotionPredictions']['emotion']
+
+    return format_response
---
+++ test_emotion_detection.py
@@ -0,0 +1,6 @@
+
+from emotion_detection import emotion_detector
+
+emotion_detector('I love this new technology.')
+
+print(test)
