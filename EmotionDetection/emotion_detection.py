f6157fbc71c411a080494ce9a8ff39bb264fa424
b-aeo <165791145+b-aeo@users.noreply.github.com>
2024-07-12 13:41:47 +0100

    added commentary

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,7 +8,9 @@ def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     text = { "raw_document": { "text": text_to_analyze } }
+    ## send a post to the ibm watson ai api to process the text in the application
     response = requests.post(url, headers = headers, json = text, timeout = 30)
+    ## error handling for error code 400, in the event of invalid user input
     if response.status_code == 400:
         empty_response = {
         'anger': None,
@@ -19,8 +21,11 @@ def emotion_detector(text_to_analyze):
         'dominant_emotion': None
         }
         return empty_response
+    ## converting api response to json
     json_response = json.loads(response.text)
+    ## parsing data
     format_response = json_response['emotionPredictions'][0]['emotion']
+    ## formatting final function response
     final_response = {
     'anger': format_response['anger'],
     'disgust': format_response['disgust'],
